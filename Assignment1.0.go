11fb5a6fae88e9254c9a5e228429c66e25f2ed51
igodiz <Lingzhuangzeng@hotmail.com>
2017-09-25 05:12:47 +0200

    Update Assignment1.0.go

--- Assignment1.0.go
+++ Assignment1.0.go
@@ -1,29 +1,28 @@
 package main
 
 import (
-"net/http"
-"log"
-"encoding/json"
-"fmt"
+	"net/http"
+	"log"
+	"encoding/json"
+	"fmt"
 )
 
 type RepoInfo struct {
-	Project 	string 		`json:"full_name"`
-	Owner		OwnerStruct 	`json:"owner`
-	LanguagesUrl 	string		`json:"languages_url"`
-	ContributorsUrl string 		`json:"contributors_url"`
-	TopContributor	Contributor
+	Project 	string 		json:"full_name"
+	Owner		OwnerStruct 	json:"owner
+	LanguagesUrl 	string		json:"languages_url"
+	ContributorsUrl string 		json:"contributors_url"
 	Languages 	[]interface{}
-
+	TopContributor	Contributor
 }
 
 type OwnerStruct struct {
-	Login string `json:"login"`
+	Login string json:"login"
 }
 
 type Contributor struct {
-	Login string `json:"login"`
-	Contributions int `json:"contributions"`
+	Login string json:"login"
+	Contributions int json:"contributions"
 }
 
 func getRepoInfo(w *http.ResponseWriter, r *http.Request, dest *RepoInfo) {
@@ -49,40 +48,47 @@ func getLanguages(w *http.ResponseWriter, r *http.Request, languageUrl string, d
 			dest.Languages = append(dest.Languages, r)
 		}
 	}
+ }
+
+ func getTopContributor(w *http.ResponseWriter, r *http.Request, dest *RepoInfo) {
+	 resp, err := http.Get(dest.ContributorsUrl)
+	 if err != nil {
+		 log.Fatal(err)
+	 }
+	 if resp.StatusCode == 200 {
+		 var contributors []Contributor
+		 json.NewDecoder(resp.Body).Decode(&contributors)
+
+		 var topContributor Contributor
+		 for i := 0; i < len(contributors); i++ {
+			 if contributors[i].Contributions > topContributor.Contributions {
+				 topContributor = contributors[i]
+			 }
+		 }
+		 dest.TopContributor.Login = topContributor.Login
+		 dest.TopContributor.Contributions = topContributor.Contributions
+	 }
+ }
+
+func PrettyPrint(w http.ResponseWriter, info RepoInfo) {
+	fmt.Fprint(w, "{\n")
+	fmt.Fprint(w, "\t\"project\": \"" + info.Project + "\",\n")
+	fmt.Fprint(w, "\t\"owner\": \"" + info.Owner.Login + "\",\n")
+	fmt.Fprint(w, "\t\"committer\": \"" + info.TopContributor.Login + "\",\n")
+	fmt.Fprintf(w,"\t\"commits\": \"%d\"%s"  ,info.TopContributor.Contributions, ",\n")
+	fmt.Fprintf(w, "\t\"languages\": \"%v%s"  ,info.Languages, "\"\n")
+	fmt.Fprint(w, "}\n")
 }
-
-func getTopContributor(w *http.ResponseWriter, r *http.Request, dest *RepoInfo) {
-	resp, err := http.Get(dest.ContributorsUrl)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if resp.StatusCode == 200 {
-		var contributors []Contributor
-		json.NewDecoder(resp.Body).Decode(&contributors)
-
-		var topContributor Contributor
-		for i := 0; i < len(contributors); i++ {
-			if contributors[i].Contributions > topContributor.Contributions {
-				topContributor = contributors[i]
-			}
-		}
-		dest.TopContributor.Login = topContributor.Login
-		dest.TopContributor.Contributions = topContributor.Contributions
-	}
-}
-
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	var repoInfo RepoInfo
-	getRepoInfo(&w, r, &repoInfo)
-	getLanguages(&w, r, repoInfo.LanguagesUrl, &repoInfo)
-	getTopContributor(&w, r, &repoInfo)
-	m, _ := json.MarshalIndent(repoInfo, "", "    ")
-	fmt.Fprint(w, string(m))
-	fmt.Fprintln(w,"hello")
+	var info RepoInfo
+	getRepoInfo(&w, r, &info)
+	getLanguages(&w, r, info.LanguagesUrl, &info)
+	getTopContributor(&w, r, &info)
+	PrettyPrint(w, info)
 }
 
 func main() {
 	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe("127.0.0.1:8083", nil)
+	http.ListenAndServe("127.0.0.1:8080", nil)
 }
