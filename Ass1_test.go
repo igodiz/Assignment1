92ec80b0388e49698f3df97ae7038d1eb0e6cbb2
igodiz <Lingzhuangzeng@hotmail.com>
2017-09-25 05:02:46 +0200

    Add files via upload

---
+++ Ass1_test.go
@@ -0,0 +1,20 @@
+package main
+
+import "testing"
+import "net/http"
+import "net/http/httptest"
+
+func Test_handler(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(handlerFunc))
+	defer ts.Close()
+
+	res, err := http.Get(ts.URL + "github.com/apache/kafka")
+	if err != nil {
+		t.Errorf("Error making the GET request, %s", err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("Expected StatusCode %d, received %d", http.StatusOK, res.StatusCode)
+		return
+	}
+}
